d1b21f4ef426c62f2a3c112db01e4a12894603df
lizeqing <lizeqing@xmfunny.com>
2022-08-29 17:21:47 +0800

    feat: 增加 xdconsole_sink_target

---
+++ internal/provider/resource_xdconsole_sink_target.go
@@ -0,0 +1,191 @@
+package provider
+
+import (
+	"context"
+	"errors"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceXDConsoleSinkTarget() *schema.Resource {
+	return &schema.Resource{
+		// This description is used by the documentation generator and the language server.
+		Description: "Sample resource in the Terraform provider scaffolding.",
+
+		CreateContext: resourceXDConsoleSinkTargetCreate,
+		ReadContext:   resourceXDConsoleSinkTargetRead,
+		UpdateContext: resourceXDConsoleSinkTargetUpdate,
+		DeleteContext: resourceXDConsoleSinkTargetDelete,
+
+		Schema: map[string]*schema.Schema{
+			"metadata": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				MinItems: 1,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+							ForceNew: true,
+						},
+						"labels": {
+							Type:     schema.TypeMap,
+							Optional: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
+			},
+			"spec": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				MinItems: 1,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"topic": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"event": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"app": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"target": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceXDConsoleSinkTargetCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*apiClient)
+
+	name := d.Get("metadata.0.name").(string)
+	labels := make(map[string]string)
+	if v, ok := d.GetOk("metadata.0.labels"); ok && v != nil {
+		for k, v := range v.(map[string]interface{}) {
+			labels[k] = v.(string)
+		}
+	}
+
+	resource := Resource{}
+	resource.Kind = "XDConsoleSinkTarget"
+	resource.Metadata.Name = name
+	resource.Metadata.Labels = labels
+
+	spec := d.Get("spec").([]interface{})[0]
+
+	resource.Spec = spec.(map[string]interface{})
+
+	d.SetId(name)
+
+	ret, err := client.Create(ctx, &resource)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("metadata", []interface{}{
+		map[string]interface{}{
+			"name":   ret.Metadata.Name,
+			"labels": ret.Metadata.Labels,
+		},
+	})
+	d.Set("spec", []interface{}{ret.Spec})
+
+	return nil
+}
+
+func resourceXDConsoleSinkTargetRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*apiClient)
+
+	kind := "XDConsoleSinkTarget"
+	name := d.Get("metadata.0.name").(string)
+
+	ret, err := client.Retrieve(ctx, kind, name)
+	if err != nil {
+		if err == errResourceNotFound {
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	d.Set("metadata", []interface{}{
+		map[string]interface{}{
+			"name":   ret.Metadata.Name,
+			"labels": ret.Metadata.Labels,
+		},
+	})
+	d.Set("spec", []interface{}{ret.Spec})
+
+	return nil
+}
+
+func resourceXDConsoleSinkTargetUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*apiClient)
+
+	name := d.Get("metadata.0.name").(string)
+	labels := make(map[string]string)
+	if v, ok := d.GetOk("metadata.0.labels"); ok && v != nil {
+		for k, v := range v.(map[string]interface{}) {
+			labels[k] = v.(string)
+		}
+	}
+
+	resource := Resource{}
+	resource.Kind = "XDConsoleSinkTarget"
+	resource.Metadata.Name = name
+	resource.Metadata.Labels = labels
+
+	spec := d.Get("spec").([]interface{})[0]
+
+	resource.Spec = spec.(map[string]interface{})
+
+	d.SetId(name)
+
+	ret, err := client.Update(ctx, &resource)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("metadata", []interface{}{
+		map[string]interface{}{
+			"name":   ret.Metadata.Name,
+			"labels": ret.Metadata.Labels,
+		},
+	})
+	d.Set("spec", []interface{}{ret.Spec})
+
+	return nil
+}
+
+func resourceXDConsoleSinkTargetDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*apiClient)
+
+	kind := "XDConsoleSinkTarget"
+	name := d.Get("metadata.0.name").(string)
+
+	err := client.Destroy(ctx, kind, name)
+	if err != nil {
+		if errors.Is(err, errResourceNotFound) {
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
---
+++ internal/provider/resource_xdconsole_sink_target_test.go
@@ -0,0 +1,46 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceXDConsoleSinkTarget(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				resource "turbine_xdconsole_sink_target" "foo" {
+					metadata {
+						name = "foo"
+					}
+					spec {
+						endpoint = "endpoint"
+						project = "project"
+						logstore = "logstore"
+						access_key_id = "access_key_id"
+						access_key_secret = "access_key_secret"
+					}
+				}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"turbine_xdconsole_sink.foo", "metadata.0.name", "foo"),
+					resource.TestCheckResourceAttr(
+						"turbine_xdconsole_sink.foo", "spec.0.endpoint", "endpoint"),
+					resource.TestCheckResourceAttr(
+						"turbine_xdconsole_sink.foo", "spec.0.project", "project"),
+					resource.TestCheckResourceAttr(
+						"turbine_xdconsole_sink.foo", "spec.0.logstore", "logstore"),
+					resource.TestCheckResourceAttr(
+						"turbine_xdconsole_sink.foo", "spec.0.access_key_id", "access_key_id"),
+					resource.TestCheckResourceAttr(
+						"turbine_xdconsole_sink.foo", "spec.0.access_key_secret", "access_key_secret"),
+				),
+			},
+		},
+	})
+}
